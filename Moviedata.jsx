daf1fc785e73d314c763ac38871cd2f69b9e0ac9
Afzal Hossain <90591676+afzalece@users.noreply.github.com>
2022-01-18 11:35:25 +0600

    Add files via upload
    Added fat arrow function to reduce length and performance of code

--- Moviedata.jsx
+++ Moviedata.jsx
@@ -36,9 +36,47 @@ const Moviedata =[
         imgsrc: "https://wallpapercave.com/wp/wp7916075.jpg",
         title : "A Amazon Orginal Series" ,
         link : "https://www.amazon.com/Dexter-Season-1/dp/B000JO9JHW"
+    },
+
+    // ========================== Create More template =================================
+    {
+        sname : 'Dark' ,
+        imgsrc: "https://wallpapercave.com/dwp1x/wp4056407.jpg" ,
+        title : "A Netflix Orginal Series" ,
+        link : "https://www.netflix.com/bd/title/80100172"
+    },
+    {
+        sname : 'Lost In Space' ,
+        imgsrc: "https://wallpapercave.com/wp/wp5271393.jpg" ,
+        title : "A Netflix Orginal Series" ,
+        link : "https://www.netflix.com/bd/title/80104198"
+    },
+    {
+        sname : 'Squid Game' ,
+        imgsrc: "https://wallpapercave.com/dwp1x/wp9920335.jpg" ,
+        title: "A Netflix Orginal Series" ,
+        link : "https://www.netflix.com/bd/title/81040344"
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp10420141.jpg" ,
+        title : "A Amazon Orginal Series" ,
+        sname : 'Dexter: New Blood', 
+        link : "https://www.amazon.com/Dexter-New-Blood-Season-1/dp/B09JCBT67H"
+    },
+
+    {
+        sname : 'The Mentalist', 
+        imgsrc: "https://wallpapercave.com/dwp1x/dJwf0vP.jpg" ,
+        title : "A Amazon Orginal Series" ,
+        link : "https://www.amazon.com/Mentalist-Complete-First-Season/dp/B002PZC8BM"
+    },
+
+    {
+        sname : 'Dexter',
+        imgsrc: "https://wallpapercave.com/wp/wp7916075.jpg",
+        title : "A Amazon Orginal Series" ,
+        link : "https://www.amazon.com/Dexter-Season-1/dp/B000JO9JHW"
     }
-
-
 
 
 
